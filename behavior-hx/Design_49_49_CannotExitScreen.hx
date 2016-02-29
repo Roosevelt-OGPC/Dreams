29807e07f804c7becf08926651385ba43335cd5f
KCantrell12 <readkpc@gmail.com>
2016-02-29 08:55:11 -0800

    test start!

--- behavior-hx/Design_49_49_CannotExitScreen.hx
+++ behavior-hx/Design_49_49_CannotExitScreen.hx
@@ -68,116 +68,48 @@ import com.stencyl.graphics.shaders.BloomShader;
 
 
 
-class ActorEvents_0 extends ActorScript
+class Design_49_49_CannotExitScreen extends ActorScript
 {
-	public var _Jump:Bool;
-	public var _Left:String;
-	public var _Right:String;
 	
 	
 	public function new(dummy:Int, actor:Actor, dummy2:Engine)
 	{
 		super(actor);
-		nameMap.set("Jump?", "_Jump");
-		_Jump = false;
-		nameMap.set("Left", "_Left");
-		nameMap.set("Right", "_Right");
+		nameMap.set("Actor", "actor");
 		
 	}
 	
 	override public function init()
 	{
 		
+		/* ======================== When Creating ========================= */
+		actor.makeAlwaysSimulate();
+		
 		/* ======================== When Updating ========================= */
 		addWhenUpdatedListener(null, function(elapsedTime:Float, list:Array<Dynamic>):Void
 		{
 			if(wrapper.enabled)
 			{
-				/* make sure actor doesn't leave screen */
 				if((actor.getScreenX() < 0))
 				{
-					actor.setX(1);
-				}
-				else if((actor.getScreenX() > (getScreenWidth() - (actor.getWidth()))))
-				{
-					actor.setX(((getScreenWidth() - (actor.getWidth())) - 1));
-				}
-			}
-		});
-		
-		/* ======================== When Updating ========================= */
-		addWhenUpdatedListener(null, function(elapsedTime:Float, list:Array<Dynamic>):Void
-		{
-			if(wrapper.enabled)
-			{
-				/* walk left */
-				if(isKeyDown("left"))
-				{
-					Engine.engine.setGameAttribute("left", true);
-					Engine.engine.setGameAttribute("right", false);
-					actor.setXVelocity(-20);
-					if(isKeyDown("shift"))
-					{
-						actor.setXVelocity(-30);
-					}
-				}
-				/* walk right */
-				else if(isKeyDown("right"))
-				{
-					Engine.engine.setGameAttribute("right", true);
-					Engine.engine.setGameAttribute("left", false);
-					actor.setXVelocity(20);
-					if(isKeyDown("shift"))
-					{
-						actor.setXVelocity(30);
-					}
-				}
-				else
-				{
+					actor.setX(getScreenX());
 					actor.setXVelocity(0);
-					Engine.engine.setGameAttribute("left", false);
-					Engine.engine.setGameAttribute("right", false);
 				}
-				/* jump */
-				if(isKeyPressed("Spacebar"))
+				if((actor.getScreenY() < 0))
 				{
-					if((_Jump == true))
-					{
-						_Jump = false;
-						propertyChanged("_Jump", _Jump);
-						actor.applyImpulseInDirection(270, 38);
-					}
+					actor.setY(getScreenY());
+					actor.setYVelocity(0);
 				}
-				_Jump = false;
-				propertyChanged("_Jump", _Jump);
-			}
-		});
-		
-		/* ======================= Member of Group ======================== */
-		addCollisionListener(actor, function(event:Collision, list:Array<Dynamic>):Void
-		{
-			if(wrapper.enabled && sameAsAny(getActorGroup(1),event.otherActor.getType(),event.otherActor.getGroup()))
-			{
-				/* jump */
-				if(!(event.thisFromTop))
+				if(((actor.getScreenX() + (actor.getWidth())) > getScreenWidth()))
 				{
-					_Jump = true;
-					propertyChanged("_Jump", _Jump);
+					actor.setX((getScreenX() + (getScreenWidth() - (actor.getWidth()))));
+					actor.setXVelocity(0);
 				}
-			}
-		});
-		
-		/* ======================== Actor of Type ========================= */
-		addCollisionListener(actor, function(event:Collision, list:Array<Dynamic>):Void
-		{
-			if(wrapper.enabled && sameAsAny(getActorType(25), event.otherActor.getType(),event.otherActor.getGroup()))
-			{
-				runLater(1000 * .5, function(timeTask:TimedTask):Void {
-					if(event.otherFromBottom)
-					{
-						createRecycledActor(getActorType(23), 235, 230, Script.MIDDLE);
-					}
-				}, actor);
+				if(((actor.getScreenY() + (actor.getHeight())) > getScreenHeight()))
+				{
+					actor.setY((getScreenY() + (getScreenHeight() - (actor.getHeight()))));
+					actor.setYVelocity(0);
+				}
 			}
 		});
 		
