71555600a55c314d1c72ac67a39821974e83eaf5
DaytonGirdner <roboticmonkey96@gmail.com>
2016-04-04 09:44:43 -0700

    Flappy bird

--- behavior-hx/SceneEvents_11.hx
+++ behavior-hx/SceneEvents_11.hx
@@ -39,6 +39,7 @@ import box2D.common.math.B2Vec2;
 import box2D.dynamics.B2Body;
 import box2D.dynamics.B2Fixture;
 import box2D.dynamics.joints.B2Joint;
+import box2D.collision.shapes.B2Shape;
 
 import motion.Actuate;
 import motion.easing.Back;
@@ -68,54 +69,18 @@ import com.stencyl.graphics.shaders.BloomShader;
 
 
 
-class ActorEvents_53 extends ActorScript
+class SceneEvents_11 extends SceneScript
 {
 	
 	
-	public function new(dummy:Int, actor:Actor, dummy2:Engine)
+	public function new(dummy:Int, dummy2:Engine)
 	{
-		super(actor);
+		super();
 		
 	}
 	
 	override public function init()
 	{
-		
-		/* ======================== When Updating ========================= */
-		addWhenUpdatedListener(null, function(elapsedTime:Float, list:Array<Dynamic>):Void
-		{
-			if(wrapper.enabled)
-			{
-				actor.setXVelocity(10);
-			}
-		});
-		
-		/* =========================== Keyboard =========================== */
-		addKeyStateListener("Spacebar", function(pressed:Bool, released:Bool, list:Array<Dynamic>):Void
-		{
-			if(wrapper.enabled && pressed)
-			{
-				actor.setYVelocity(-25);
-			}
-		});
-		
-		/* =========================== Keyboard =========================== */
-		addKeyStateListener("Spacebar", function(pressed:Bool, released:Bool, list:Array<Dynamic>):Void
-		{
-			if(wrapper.enabled && pressed)
-			{
-				actor.setAnimation("" + "Animation 0");
-			}
-		});
-		
-		/* ======================== Something Else ======================== */
-		addCollisionListener(actor, function(event:Collision, list:Array<Dynamic>):Void
-		{
-			if(wrapper.enabled)
-			{
-				recycleActor(actor);
-			}
-		});
 		
 	}
 	
